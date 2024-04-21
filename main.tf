0be05bc5a3cb16ca199ef5f426842cf58b85b0a3
Dhananjay Lakkawar <dlakkawar@localhost.localdomain>
2024-04-21 15:54:40 +0530

    version 1.2

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "google" {
+  project = "my-project-540-405910"
+}
+
 resource "google_storage_bucket" "my_bucket" {
   name                     = "t-githubdemo-bucket"
   location                 = "US"
