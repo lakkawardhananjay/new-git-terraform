76b190f1346770e12ca468a3d5c377e0a08844bd
Dhananjay Lakkawar <dlakkawar@localhost.localdomain>
2024-04-21 15:34:26 +0530

    version 1.1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "google_storage_bucket""my_bucket"{
-  name="t-githubdemo-bucket"
-  location ="US"
-  force_destroy=true
-  public_access_prevention="enforced"
+resource "google_storage_bucket" "my_bucket" {
+  name                     = "t-githubdemo-bucket"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
 }
